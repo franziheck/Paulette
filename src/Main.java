a06351b1601c06a4365072cb38ed8d28404160d3
Unknown <r.erdweg@gmail.com>
2017-09-17 20:07:18 +0200

    Grundgerüst des Bots

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,22 @@
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
 public class Main {
 
-    
+    public static void main (String[] args){
+        ApiContextInitializer.init();
+        registerBots();
+    }
+
+    private static void registerBots(){
+        TelegramBotsApi botsApi = new TelegramBotsApi();
+
+        try{
+            botsApi.registerBot(new Paulette());
+            System.out.println("Success! BotServer connected to Telegram Server");
+        } catch (TelegramApiException e) {
+            System.out.println("Unable to connect to Telegram Server. Please check BotToken and Internet Settings");
+        }
+    }
 }
