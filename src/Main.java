b3034c5217c490933283ead472bfb4c066ad4824
Unknown <r.erdweg@gmail.com>
2017-09-27 21:24:46 +0200

    included PropsHanlder

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,25 @@
+import org.paulette.res.PropsHandler;
 import org.telegram.telegrambots.ApiContextInitializer;
 import org.telegram.telegrambots.TelegramBotsApi;
 import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+import java.io.File;
 
 public class Main {
 
     public static void main (String[] args){
         ApiContextInitializer.init();
         registerBots();
+    }
+
+    private static void initializeServer(){
+
+        if(new File("config.properties").isFile()){
+            System.out.println("Welcome Back");
+        }else{
+            System.out.println("Created config.properties");
+            PropsHandler.createProperties("", false);
+        }
     }
 
     private static void registerBots(){
